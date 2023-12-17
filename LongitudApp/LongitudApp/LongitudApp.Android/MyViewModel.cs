1141f8584053f43935093a2c983b44af650da61b
AlbaZ <129638361+Albaez@users.noreply.github.com>
2023-12-17 10:51:26 -0600

    Add files via upload
    agregando archivos qu VS no cargo automatico.

---
+++ LongitudApp/LongitudApp/LongitudApp.Android/MyViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+
+namespace LongitudApp
+{
+    public class KmToMViewModel : INotifyPropertyChanged
+    {
+        private double valorKm;
+        private double resultado;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double ValorKm
+        {
+            get { return valorKm; }
+            set
+            {
+                if (valorKm != value)
+                {
+                    valorKm = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double Resultado
+        {
+            get { return resultado; }
+            set
+            {
+                if (resultado != value)
+                {
+                    resultado = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public CommandID ConvertirKmCommand { get; }
+
+        public KmToMViewModel()
+        {
+            ConvertirKmCommand = new CommandID (ConvertirKm);
+        }
+
+        private void ConvertirKm()
+        {
+            Resultado = ValorKm * 1000;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ LongitudApp/LongitudApp/LongitudApp.Android/TabbedPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LongitudApp.Droid
+{
+
+    public partial class TabbedPage : MainActivity
+    {
+        public TabbedPage ()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
